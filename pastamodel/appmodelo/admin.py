619fc4d23b9147e3f2a48b9585552594d9c4a51c
Pgabriel2605 fortunato2605 <pedrogabrielfortunato2016@gmail.com>
2024-04-08 17:43:08 +0000

    Genero, Autor, Livro, Editora

--- pastamodel/appmodelo/admin.py
+++ pastamodel/appmodelo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Genero, Editora, Autor, Livro
 
 # Register your models here.
+admin.site.Register(Genero)
+admin.site.Register(Editora)
+admin.site.Register(Autor)
+admin.site.Register(Livro)
